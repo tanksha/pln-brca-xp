42de7fc114aa5c3a5e15a50432a6ed19873b4e7a
Abdulrahman Semrie <hsamireh@gmail.com>
2020-11-13 17:54:07 +0300

    Support both overexpression and underexpression

--- pln-bio/bp-deduction.scm
+++ pln-bio/bp-deduction.scm
@@ -17,7 +17,7 @@
 (define PT (Type "PredicateNode"))
 (define BT (Type "BiologicalProcessNode"))
 
-(define-public (generate-patient-bp-link-rule)
+(define-public (generate-patient-bp-link-rule-overexpr)
     (cog-execute! (Bind 
         (VariableList 
             (TypedVariable patient-var CT)
@@ -37,7 +37,7 @@
                     (Member (Variable "$gene") bp-var))))
             
         (ExecutionOutputLink
-            (GroundedSchemaNode "scm: generate-subset-tv")
+            (GroundedSchemaNode "scm: generate-subset-tv-overexpr")
             (ListLink
                 (Context
                     (Concept "profiled-genes")
@@ -53,12 +53,48 @@
                 patient-var
                 bp-var)))))
 
-(define-public (generate-subset-tv conclusion . premises)
+(define-public (generate-patient-bp-link-rule-underexpr)
+    (cog-execute! (Bind 
+        (VariableList 
+            (TypedVariable patient-var CT)
+            (TypedVariable bp-var BT))
+        (AndLink 
+            (Present 
+                patient-var
+                bp-var)
+            (SatisfactionLink 
+                (TypedVariable (Variable "$gene") (Type "Gene"))
+                (Present 
+                    (EvaluationLink
+                        (LazyExecutionOutputLink
+                            (SchemaNode "make-underexpression-predicate-for-gene")
+                            (VariableNode "$gene"))
+                            patient-var)               
+                    (Member (Variable "$gene") bp-var))))
+            
+        (ExecutionOutputLink
+            (GroundedSchemaNode "scm: generate-subset-tv-underexpr")
+            (ListLink
+                (Context
+                    (Concept "profiled-genes")
+                    (Subset              
+                        bp-var
+                        (SatisfyingSetScope
+                            (Variable "$G")
+                            (EvaluationLink
+                            (LazyExecutionOutputLink
+                                (SchemaNode "make-underexpression-predicate-for-gene")
+                                (VariableNode "$G"))
+                                patient-var))))                
+                patient-var
+                bp-var)))))
+
+(define-public (generate-subset-tv-overexpr conclusion . premises)
     (if (= (length premises) 2)
         (let* ((patient (car premises))
                (go-term (cadr premises))
                (A-mbrs (get-profiled-go-mbrs go-term))
-               (numt (numerator A-mbrs patient))
+               (numt (numerator-overexpr A-mbrs patient))
                (dnt (denominator A-mbrs))
                (tvs (if (< 0 dnt) (exact->inexact (/ numt dnt)) 1))
                (tvc (count->confidence (length A-mbrs)))
@@ -66,6 +102,19 @@
                
             (if (< 0 tvc) (cog-merge-hi-conf-tv! conclusion TV)))))
 
+
+(define-public (generate-subset-tv-underexpr conclusion . premises)
+    (if (= (length premises) 2)
+        (let* ((patient (car premises))
+               (go-term (cadr premises))
+               (A-mbrs (get-profiled-go-mbrs go-term))
+               (numt (numerator-underexpr A-mbrs patient))
+               (dnt (denominator A-mbrs))
+               (tvs (if (< 0 dnt) (exact->inexact (/ numt dnt)) 1))
+               (tvc (count->confidence (length A-mbrs)))
+               (TV (stv tvs tvc)))
+               
+            (if (< 0 tvc) (cog-merge-hi-conf-tv! conclusion TV)))))
 
 (define (get-profiled-go-mbrs A)
     (cog-outgoing-set (cog-execute! 
@@ -95,7 +144,7 @@
 ;     X)
 ;   B))
 
-(define-public (numerator A-mbrs B)
+(define-public (numerator-overexpr A-mbrs B)
     (define (fuzzy-intersect A-mbr)
         (let* ((execution-ln (cog-link 'LazyExecutionOutputLink (SchemaNode "make-overexpression-predicate-for-gene") A-mbr))
                (B-eval (cog-link 'EvaluationLink execution-ln B)))
@@ -105,6 +154,16 @@
 
     (fold + 0 (map fuzzy-intersect A-mbrs)))
 
+
+(define-public (numerator-underexpr A-mbrs B)
+    (define (fuzzy-intersect A-mbr)
+        (let* ((execution-ln (cog-link 'LazyExecutionOutputLink (SchemaNode "make-underexpression-predicate-for-gene") A-mbr))
+               (B-eval (cog-link 'EvaluationLink execution-ln B)))
+            (if (null?  B-eval)
+                0
+                (min (cog-mean A-mbr) (cog-mean B-eval)))))
+
+    (fold + 0 (map fuzzy-intersect A-mbrs)))
 
 ;; Given evaluation links of B calculate the fuzzy sum of 
 ;;the patterns of B expressed as 
@@ -122,20 +181,3 @@
             (Present 
                 (Member (Variable "$g") go)
                 (Member (Variable "$g") (ConceptNode "profiled-genes")))))))
-
-(define-public (find-patient-overexpr-genes patient)
-    (cog-outgoing-set (cog-execute! 
-        (Bind 
-            (VariableList 
-                (TypedVariable (Variable "$g") (Type "GeneNode")))
-            (Present
-                (Evaluation 
-                    (LazyExecutionOutputLink
-                        (SchemaNode "make-overexpression-predicate-for-gene")
-                        (Variable "$g"))
-                    patient))
-            (Evaluation 
-                (LazyExecutionOutputLink
-                    (SchemaNode "make-overexpression-predicate-for-gene")
-                    (Variable "$g"))
-                patient)))))
